eb2b98b7ca14b4b6721fd03db54e6e4cebbc5f92
Graham Hall <graham@ghall.dev>
2024-05-27 15:48:55 -0400

    setup alpinejs

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,28 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style/fonts");
+
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
+  });
+
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith(".html")
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
