be703fcb693c65bf47ccbcf8f71e4227e842696f
Graham Hall <graham@ghall.dev>
2024-05-28 19:15:34 -0400

    reworked hoempage

--- .eleventy.js
+++ .eleventy.js
@@ -2,18 +2,14 @@ const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style/fonts");
-  eleventyConfig.addPassthroughCopy("src/icons");
+  eleventyConfig.addPassthroughCopy("src/img");
 
   eleventyConfig.addPassthroughCopy({
     "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
   });
 
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    if (
-      process.env.ELEVENTY_PRODUCTION &&
-      outputPath &&
-      outputPath.endsWith(".html")
-    ) {
+  eleventyConfig.addTransform(htmlmin, function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
